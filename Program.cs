5422ea5ab306d5587f90d2580450f6528dc968eb
smzimela <smzimela@student.wethinkcode.co.za>
2023-01-15 22:52:48 +0200

    token changed

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Slack_App
     {
         static void Main(string[] args)
         {
-            string token = "xoxb-4629455833220-4658671317777-9czZkjAbZAeu2olb078oZBO1";
+            string token = "xoxb-4629455833220-4658671317777-kLdleXZEg6n3IwPgxCidvx7w";
             var writer = new SlackMessageWriter(token);
             Task.WaitAll(writer.WriteMessage("C# test message-scanner app"));
         }
