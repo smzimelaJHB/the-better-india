ceda1057b66510f353ecd4ba954eaf663879f16f
smzimela <smzimela@student.wethinkcode.co.za>
2023-01-15 22:48:45 +0200

    write into the-better-india channel

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Slack_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string token = "xoxb-4629455833220-4658671317777-9czZkjAbZAeu2olb078oZBO1";
+            var writer = new SlackMessageWriter(token);
+            Task.WaitAll(writer.WriteMessage("C# test message-scanner app"));
+        }
+    }
+}
